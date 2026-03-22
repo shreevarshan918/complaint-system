4b5308b99aefc0c1c1a3e8d3fd2f31c570553647
Shreevarshan918 <shreevarshnyj@gmail.com>
2026-03-22 06:38:24 +0530

    fix port binding

--- app.py
+++ app.py
@@ -199,4 +199,7 @@ def logout():
     return redirect("/login")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    from waitress import serve
+    port = int(os.environ.get("PORT", 5000))
+    serve(app, host="0.0.0.0", port=port)
